889a688e11ac344d43a9ab04944167fb3831e056
Shaquille Mercado <shaquillesma04@gmail.com>
2023-09-29 21:06:26 -0400

    Entidad Producto ya lista su interfaz

---
+++ SalesOnline/SalesOnline.Infraestructure/Repository/ProductoRepository.cs
@@ -0,0 +1,36 @@
+﻿using SalesOnline.Domain.Entities;
+using SalesOnline.Infraestructure.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SalesOnline.Infraestructure.Repository
+{
+    public class ProductoRepository : IProductoRepository
+    {
+        public List<Producto> GetEntities()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Producto GetEntity(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(Producto entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save(Producto entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Producto entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
