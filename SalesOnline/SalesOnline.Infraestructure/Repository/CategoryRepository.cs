b97d826e38e4b24420510d5016e9f5e6d074852a
MykeLora <maycollora16@gmail.com>
2023-09-29 20:51:43 -0600

    La entidad categoria ya tiene su interface

---
+++ SalesOnline/SalesOnline.Infraestructure/Repository/CategoryRepository.cs
@@ -0,0 +1,41 @@
+﻿using SalesOnline.Domain.Entities;
+using SalesOnline.Infraestructure.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SalesOnline.Infraestructure.Repository
+{
+    public class CategoryRepository : ICategoyRepository
+    {
+        public List<Category> GetEntities()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Category GetEntity(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Producto> GetProductosbyid(int idcategoria)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(Category entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save(Category entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Category entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
