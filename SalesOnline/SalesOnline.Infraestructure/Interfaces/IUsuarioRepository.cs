f5160e6c8fe3e2ee34d841db1ae371dea061d9f4
ancheloroman23 <ancheloroman23@gmail.com>
2023-12-06 22:22:58 -0400

    #

--- SalesOnline/SalesOnline.Infraestructure/Interfaces/IUsuarioRepository.cs
+++ SalesOnline/SalesOnline.Infraestructure/Interfaces/IUsuarioRepository.cs
@@ -8,6 +8,6 @@ namespace SalesOnline.Infraestructure.Interfaces
     public interface IUsuarioRepository : IBaseRepository<Usuario>
     {
         public List<UsuarioModel> GetUsuarios();
-        public UsuarioModel GetUsuario(int idUsuario);
+        public UsuarioModel GetUsuario(int id);
     }
 }
